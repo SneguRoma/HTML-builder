eadbec51e255e2edca330ed535785a9ff935c5d5
sneguroma <sneguroma@gmail.com>
2022-11-04 18:33:51 +0100

    feat: task 2 complete

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,42 +6,21 @@ const rl = readline.createInterface({ input, output });
 const fileWright = path.join(__dirname, 'text.txt');
 
 const stream = fs.createWriteStream(fileWright, 'utf-8'); 
+output.write('Введите текст\n');
 
-
-rl.question('Введите текст:\n', (answer) => {
-  //console.log(typeof answer)
-  function newLine(){
-    rl.on('line', (input) => {
-        if(input.indexOf("exit") != -1){            
-            rl.close(); 
-            process.exit(); 
-        }
-        else{
-          stream.write(input);
-          newLine();  
-        }
-    });
-     
-  }
-  if (answer.indexOf("exit") != -1){    
-    rl.close();
-    process.exit();
-  }
-   else {
-    stream.write(answer); 
-    newLine();
-  }  
+rl.on('line', (input) => {
+  newLine(input); 
 });
+
+function newLine(input){
+  if(input.indexOf("exit") != -1){            
+    rl.close(); 
+    process.exit(); 
+  }
+  else{
+    stream.write(input);     
+  }
+}   
+
 process.on('exit', () => output.write('Удачи!'));
-
-
-
-
-
-/* stdout.write('Введите текст:\n');
-stdin.on('data', data => {
-    const textString = data.toString();
-    const reverseName = textString.split('').reverse().join('');
-    stdout.write(`\nТвоё имя наоборот ${reverseName}`);
-    process.exit();
-  }); */
+stream.on('error', error => console.log('Error', error.message));
