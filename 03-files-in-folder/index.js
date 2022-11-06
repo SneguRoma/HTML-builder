61107cd58fc5431b991458e1121b8aa766cf0379
sneguroma <sneguroma@gmail.com>
2022-11-06 15:33:08 +0100

    feat: task 3 complete

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const { serialize } = require('v8');
+
+
+const dirRead = path.join(__dirname, 'secret-folder');
+let dirEntries = [];
+
+fs.readdir(dirRead, {withFileTypes: true}, function(err, stats) {
+    //console.log(stats);
+    for(let file of stats){
+        if(file.isFile()){
+        dirEntries.push(file);
+        let stat;
+        let pathFile = path.join(__dirname, 'secret-folder', file.name); 
+        fs.stat(pathFile, function(err, stats) {
+             stat = stats.size;
+             //console.log(stat);
+             console.log(file.name.split('.').join(' - ') + ' - ' + stats.size + 'b'); 
+            } );         
+        }         
+    }    
+     
+} ); 
+    
+/* for(let file of dirEntries){
+    let stat;
+    let pathFile = path.join(__dirname, 'secret-folder', file.name); 
+    //fs.stat(pathFile, function(err, stats) {
+    // stat = stats.size;
+    // console.log(stat);} );
+    if(file.isFile()){    
+     console.log(file.name.split('.').join('-')+ '-' + stat);   
+    }
+    
+} */
+   
+
+//console.log(dirEntries);
+  
