b63002fccd486b3fb894c66a8cb436993215101a
sneguroma <sneguroma@gmail.com>
2022-11-07 22:59:04 +0100

    feat: task 6 complete

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -17,7 +17,7 @@ const indexWright = path.join(dirWrite, 'index.html');
 
 
 
-let dirEntries = [];
+
 let data = '';
 let indexResult = '';
 let article = '';
@@ -32,7 +32,7 @@ fsPromises.mkdir(dirWrite, {recursive: true}).then(function() {
       if (err) throw err;
       for(let file of stats){
           if(file.isFile()){
-              dirEntries.push(file);        
+                      
               let readFile = path.join(components, file.name);               
               let stream = fs.createReadStream(readFile, 'utf-8');
               let component = '';
@@ -49,34 +49,111 @@ fsPromises.mkdir(dirWrite, {recursive: true}).then(function() {
               stream.on('error', error => console.log('Error', error.message));                                      
           }        
       }
-      
-
-  } );
-
-
-
-
-
-
-    
- /*      articleStream.on('data', chunk => article += chunk);
-      articleStream.on('error', error => console.log('Error', error.message));
-      articleStream.on('end', 
-      function(){
-        headerStream.on('data', chunk => header += chunk);
-        headerStream.on('error', error => console.log('Error', error.message));
-        headerStream.on('end', function(){
-          footerStream.on('data', chunk => footer += chunk);
-          footerStream.on('error', error => console.log('Error', error.message));
-          footerStream.on('end', function(){
-            dirEntries = data.split('{{header}}').join(header).split('{{articles}}').join(article).split('{{footer}}').join(footer);
-            indexStream.write(dirEntries);
-            //console.log(dirEntries);
-          });
-        });
-        
-      }); */
-     
+  } );     
   });
   
+});
+
+
+const fileWright = path.join(__dirname, 'project-dist', 'style.css');
+const dirRead = path.join(__dirname, 'styles');
+
+
+const writeStream = fs.createWriteStream(fileWright, 'utf-8');
+
+fs.readdir(dirRead, {withFileTypes: true}, function(err, stats) {    
+    if (err) throw err;
+    for(let file of stats){        
+        if(file.isFile() && path.extname(file.name)==='.css'){                   
+            let readFile = path.join(dirRead, file.name); 
+            let stream = fs.createReadStream(readFile, 'utf-8');
+            let data = '';
+            stream.on('data', chunk => data += chunk);            
+            stream.on('end', () => writeStream.write(data));
+            stream.on('error', error => console.log('Error', error.message)); 
+            writeStream.on('error', error => console.log('Error', error.message));                         
+        }        
+    }
+} );  
+
+
+const assetsRead = path.join(__dirname, 'assets');
+const assetsWrite = path.join(dirWrite, 'assets');
+let dirEntries = [];
+let fileEntries = [];
+fsPromises.mkdir(assetsWrite, {recursive: true}).then(function() {   
+    fs.readdir(assetsWrite, {withFileTypes: true}, function(err, writes) {       
+        if (err) throw err;
+        for(let file of writes){
+          if(file.isFile()){
+            let deleteFile = path.join(assetsWrite, file.name);
+            fs.unlink(deleteFile,function(err, deleted){
+                if (err) throw err;
+            } ); 
+          }
+          else{                              
+            let assetsInstWrite = path.join(assetsWrite, file.name);
+            fs.readdir(assetsInstWrite, {withFileTypes: true}, function(err, writes) {       
+              if (err) throw err;
+              for(let file of writes){
+                if(file.isFile()){
+                  let deleteFile = path.join(assetsInstWrite, file.name);
+                  fs.unlink(deleteFile,function(err, deleted){
+                      if (err) throw err;
+                  } ); 
+                }
+          }                    
+                           
+        });
+      }                
+    }
+   } );
+}).then(function () {
+       fs.readdir(assetsRead, {withFileTypes: true}, function(err, stats) {    
+            if (err) throw err;
+            for(let file of stats){
+                if(file.isFile()){
+                    //dirEntries.push(file);        
+                    let readFile = path.join(assetsRead, file.name); 
+                    let writeFile = path.join(assetsWrite, file.name);
+                    let stream = fs.createReadStream(readFile, 'utf-8');
+                    let data = '';
+                    stream.on('data', chunk => data += chunk); 
+                    let writeStream = fs.createWriteStream(writeFile, 'utf-8');
+                    stream.on('end', () => writeStream.write(data));
+                    stream.on('error', error => console.log('Error', error.message)); 
+                    writeStream.on('error', error => console.log('Error', error.message));                         
+                }
+                else{
+                  let assetsInst = path.join(assetsRead, file.name);                  
+                  let assetsInstWrite = path.join(assetsWrite, file.name);                  
+                  fsPromises.mkdir(assetsInstWrite, {recursive: true}).then(function(){
+                    fs.readdir(assetsInst, {withFileTypes: true}, function(err, stats) {    
+                    if (err) throw err;
+                    
+                    for(let file of stats){
+                        if(file.isFile()){
+                                  
+                            let readFile = path.join(assetsInst, file.name); 
+                            let writeFile = path.join(assetsInstWrite, file.name);
+                            let stream = fs.createReadStream(readFile, 'utf-8');
+                            let data = '';
+                            stream.on('data', chunk => data += chunk);
+                             
+                            let writeStream = fs.createWriteStream(writeFile, 'utf-8');
+                           stream.on('end', () => writeStream.write(data));
+                            stream.on('error', error => console.log('Error', error.message)); 
+                            writeStream.on('error', error => console.log('Error', error.message));                         
+                        }
+                    }
+                }); 
+              });
+                         
+              }
+            
+            }
+        } ); 
+
+}).catch(function() {
+    console.log('failed to create directory');
 });
